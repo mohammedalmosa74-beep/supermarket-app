19f23916488586dbfd982cdf818a93a557d6a2ec
mohammedalmosa74-beep <mohammedalmosa74@gmail.com>
2026-08-02 08:31:23 +0300

    Add server/public/sw.js

---
+++ server/public/sw.js
@@ -0,0 +1,24 @@
+var CACHE = 'supermarket-v3';
+var urls = ['/', '/customer.html?v=2', '/admin.html?v=2', '/manifest.json'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(urls); }).then(function() { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); }));
+    }).then(function() { return clients.claim(); })
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        var copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }))
+  );
+});
